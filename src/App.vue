575da6339782be62749243ab15274449c1539c7a
Laboratorynotices <muenchen.webas@gmail.com>
2022-08-28 12:33:16 +0200

    Добавлена структура данных для проектов

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
+  {{ projects }}
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const projects =
+  [
+    {
+      'name': 'Vogelsberg',
+      'url': 'google.com',
+      'description': 'Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. ',
+      'technologies' : ['vue', 'html'],
+    },
+    {
+      'name': 'Steinhaufen',
+      'url': 'google.de',
+      'description': 'Aha-hu-ha ahu uha. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. ',
+      'technologies' : ['cakephp', 'html'],
+    },
+    {
+      'name': 'Wassertahl',
+      'url': 'google.at',
+      'description': 'Tropf-Tropf, Tropf, Tropf, Tropf-Tropf. Tropf.',
+      'technologies' : ['yii2', 'html'],
+    },
+  ]
+
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
