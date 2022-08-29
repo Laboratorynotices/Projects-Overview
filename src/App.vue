634078defccccd4469b7b8c0f750a5a1e11f9997
Laboratorynotices <muenchen.webas@gmail.com>
2022-08-29 17:14:29 +0200

    Экспорт списка технологий из проектов.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+
+  {{ technologies }}
+  
   <img alt="Vue logo" src="./assets/logo.png">
   <ProjectsSection :projects="projects" />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -29,6 +32,37 @@ const projects =
       technologies : ['yii2', 'html'],
     },
   ]
+
+const technologies = projects.map(function (project) {
+    // Вытягиваем массивы с технологиями из списка проектов.
+    return (project.technologies)
+    // На выходе получаются двухмерные массивы технологий.
+  }).reduce(function(pre, cur) {
+    // Переделываем двухмерный массив в одномерный, но более длинный
+    return pre.concat(cur)
+    // На выходе получается одномерный массив с технологиями, но значения могут быть двойными
+  }).reduce(function(pre, cur) {
+    // Избавляемся от двойных значений в массиве
+
+    // При первом проходе в аккамуляторе "pre" находится простая строчка String,
+    // для нормальной работы превращаем его в массив.
+    if (typeof pre === 'string' || pre instanceof String)
+      pre = [pre]
+
+    // Проверка есть ли это значение уже в результате
+    if (pre.includes(cur)) {
+      // Да, нынешний элемент уже находится в списке.
+      // Ничего не делаем, просто возвращаем аккамулятор.
+      return pre
+    } else {
+      // Этого значения ещё нет в списке,
+      // Добавляем его в список.
+      return pre.concat(cur)
+    }
+
+    // На выходе получается список технологий без повторений.
+  })
+
 
 </script>
 
